7228b988e15eca3532bcb416076ea66342f8c2f5
TillWurner <85416653+TillWurner@users.noreply.github.com>
2023-01-02 02:07:40 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+      <meta charset="UTF-8">
+      <title>Proyect</title>
+      <link rel="stylesheet" href={{ asset('contact-us/contactus.css') }}>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    </head>
+    <body>
+        <div class="hero">
+            <div class="text" style="display: inline-block">
+                <h1 class="aws" style=" margin-bottom: 350px;">Principales etapas para el desarrollo de un software!</h1>
+             </div>
+            <nav>
+                <img src={{ asset('welcome/img/logo8.png') }} class="logo">
+                <ul>
+                    <li><a href="#">¡ Bienvenido !</a></li>
+                    {{-- <li><a href="#">Log In</a></li>
+                    <li><a href="#">Register</a></li>
+                    <li><a href="#">Contact Us</a></li> --}}
+                </ul>
+            </nav>
+        </div>
+        <!--act-->
+        <!-- scripts -->
+        <!-- bootstrap javascript cdn -->
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <div class="container">
+            <div class="card">
+                <div class="face face1">
+                    <div class="content">
+                        <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                        <h3>1er Paso</h3>
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content">
+                        <p>
+                            Primero se debe diseñar la estructura del sistema o software que se desea desarrollar, 
+                            es primordial empezar por este paso al momento de desarrollar softwares.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="face face1">
+                    <div class="content">
+                        <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true">
+                        <h3>2nd Paso</h3>
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content">
+                        <p>
+                        Una vez planeada las etapas del desarrollo se procede a codificar, se debe distribuir de manera uniforme
+                        las etapas entre todos los demas integrantes del equipo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="face face1">
+                    <div class="content">
+                        <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true">
+                        <h3>3er Paso</h3>
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content">
+                        <p>
+                            Finalmente cuando se concluyo con todas las etapas planeadas y codificadas se procede a hacer
+                            un ultimo testeo para posteriormente pasarlo a producción.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
